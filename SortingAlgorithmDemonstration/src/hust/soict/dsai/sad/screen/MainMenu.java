e41ef81fbea573532e86056a2764f7196b35b5e1
leducanh112 <duca1122001@gmail.com>
2021-05-27 23:22:10 +0700

    commmit

--- SortingAlgorithmDemonstration/src/hust/soict/dsai/sad/screen/MainMenu.java
+++ SortingAlgorithmDemonstration/src/hust/soict/dsai/sad/screen/MainMenu.java
@@ -1,9 +1,15 @@
 package hust.soict.dsai.sad.screen;
+
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
 public class MainMenu extends JFrame {
 	public MainMenu() {
@@ -27,13 +33,36 @@ public class MainMenu extends JFrame {
 		
 		setVisible(true);
 		setTitle("Sorting Algorithm Demonstration");
-		setSize(400, 200);
+		setSize(400, 200);	
+		
+		ButtonListener btnListener = new ButtonListener();
+		HelpMenu.addActionListener(btnListener);
+		ExitButton.addActionListener(btnListener);
 	}
-
+	
+	private class ButtonListener implements ActionListener{
+		private JPanel panel;
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String button =e.getActionCommand();
+			if (button.equals("Exit")) {
+				int n = JOptionPane.showConfirmDialog(
+			            panel, "Are you sure you want to exit?", 
+			            "Exit", JOptionPane.YES_NO_OPTION);
+				if(n == JOptionPane.YES_OPTION)
+					System.exit(0);
+			}
+			if (button.equals("Help")) {					
+				JOptionPane.showMessageDialog(panel, "Array is the most basic structure of computer science.\r\n" + 
+			"Most operations as well as other data structures are built and performed on array. \r\n" +
+						"This program is to  explain three sorting algorithms on array: bubble sort, quicksort and insertion sort.", "Help", 
+                        JOptionPane.INFORMATION_MESSAGE);
+			}
+		}
+	}							
+	
 	public static void main(String[] args) {
 		new MainMenu();
 		// TODO Auto-generated method stub
-
 	}
-
 }
